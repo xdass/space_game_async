97d9a76210decfa876276310c1ec89fa66d4195a
dass <420600base>
2019-04-12 09:20:30 +0500

    Refactor border checks.Removed unused function.

--- animations/spaceship_animation.py
+++ animations/spaceship_animation.py
@@ -7,32 +7,19 @@ GameField = namedtuple("GameField", "x1 y1 x2 y2")
 SpaceShip = namedtuple("SpaceShip", "x1 y1 x2 y2")
 
 
-def is_ship_inside(field, sx1, sy1, sx2, sy2):
-
-    left_corner = sx1 > field.x1 and sx1 < field.x2 and sy1 > field.y1 and sy1 < field.y2
-    right_corner = sx2 > field.x1 and sx2 < field.x2 and sy2 > field.y1 and sy2 < field.y2
-    return left_corner and right_corner
-
-
 async def animate_spaceship(canvas, frames):
     max_row, max_col = canvas.getmaxyx()
     frame_max_row, frame_max_col = get_frame_size(frames[0])
     row = max_row - (frame_max_row + 1)
     col = max_col // 2
-    game_field = GameField(x1=0, y1=0, x2=max_col, y2=max_row)
     while True:
         dy, dx, space = read_controls(canvas)
-        row_border = row + frame_max_row
-        col_border = col + frame_max_col
-        prev_x = col
-        prev_y = row
-        if (0 < col + dx) < (max_col - frame_max_col):
+        new_col = col + dx
+        new_row = row + dy
+        if 0 < new_col < (max_col - frame_max_col):
             col += dx
-        if (0 < row + dy) < (max_row - frame_max_row):
+        if 0 < new_row < (max_row - frame_max_row):
             row += dy
-        # if not is_ship_inside(game_field, col, row, col + frame_max_col, row + frame_max_row):
-        #     col = prev_x
-        #     row = prev_y
 
         for frame in frames:
             draw_frame(canvas, row, col, frame)
