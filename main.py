45e159350f1cd9545bb243ea4d9bd3dc069b8667
dass <420600base>
2019-04-15 16:46:09 +0500

    Add garbage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from animations.fire_animation import fire
 from animations.stars_animation import blink
 from animations.spaceship_animation import animate_spaceship
+from animations.fill_orbit import fill_orbit_with_garbage
 import time
 from random import randint, choice
 import curses
@@ -9,6 +10,7 @@ from curses import curs_set
 
 TIC_TIMEOUT = 0.1
 STARS_AMOUNT = 120
+coroutines = list()
 
 
 def get_window_size():
@@ -22,16 +24,17 @@ def get_star():
 
 
 def draw(canvas):
+    global coroutines
     curs_set(False)
     canvas.nodelay(True)
     canvas.border()
 
     ship_frames = load_ship_frames()
     row, col = get_window_size()
-    coroutines = list()
     coroutines.append(animate_spaceship(canvas, ship_frames))
     coroutines.extend([blink(canvas, randint(1, row-2), randint(1, col-2), get_star()) for _ in range(STARS_AMOUNT)])
-    
+    coroutines.extend(fill_orbit_with_garbage(canvas) for _ in range(1, 5))
+
     while True:
         for coroutine in coroutines:
             try:
